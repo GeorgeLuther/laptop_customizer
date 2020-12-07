db6deec6b7e5af461101d0c41b4f75d926556f2f
George Luther <george_l@thinkful.com>
2020-12-06 21:18:37 -0500

    error fixed in parts label, this branch is now working

--- src/components/app/mainForm/Parts.js
+++ src/components/app/mainForm/Parts.js
@@ -11,13 +11,12 @@ export default class Parts extends React.Component {
                     className="feature__option"
                     name={slugify(this.props.feature)}
                     checked={this.props.item.name === this.props.selected[this.props.feature].name}
-
                     onChange={e => {
                         console.log('its changing')
                         this.props.handleItemChange(this.props.feature, this.props.item)
                     }}
                 />
-                <label htmlFor={this.props.itemHash} className="feature__label">
+                <label htmlFor={this.props.id} className="feature__label">
                     {this.props.item.name} ({this.props.USCurrencyFormat.format(this.props.item.cost)})
                 </label>
             </div>
